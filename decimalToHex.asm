f5231e413c62b28b4191bec604278dbd5a1e327a
md-mostafa <md.mostafa.akash@gmail.com>
2021-09-06 21:59:58 +0600

    decimalToHex

---
+++ decimalToHex.asm
@@ -0,0 +1,78 @@
+; multi-segment executable file template.
+
+data segment
+    ; add your data here!
+    pkey db "press any key...$"
+    i db ?
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+
+    ; add your code here           
+    mov ax, 15d
+    mov dl, 16d
+    mov i, 0d
+    
+    L1:
+    div dl
+    ;cmp ah, 0h
+    ;je print
+    
+    mov bh, 0h
+    mov bl, al
+    
+    
+    mov ch, 0h
+    mov cl, ah
+    push cx
+    inc i
+    
+    cmp al, 0h
+    je print
+    
+    
+    mov ax, bx
+    mov dl, 16d
+    
+    jmp L1
+    
+    
+    print:
+    cmp i, 0d
+    je exit
+    
+    pop cx
+    
+    mov dl, cl  
+    cmp dl, 0Ah
+    jge printHexDigit  
+    add dl, 48d
+    int 21h  
+    again:
+    mov ah, 2
+    int 21h
+    
+    
+    
+    dec i
+    jmp print
+    
+    printHexDigit:
+    add dl, 55d
+    jmp again
+            
+    exit:
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+
+end start ; set entry point and stop the assembler.
